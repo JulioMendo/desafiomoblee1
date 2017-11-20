9b17cd5c92b822fe0ca0be95ac5a486d4166c653
Julio Cezar Mendo <mendo.julio.c@gmail.com>
2017-11-19 22:47:01 -0200

    Finished v1 of .php function

--- callFromApi.php
+++ callFromApi.php
@@ -5,14 +5,12 @@ $api = new PokeApi;
 
 $all_poke = array();
 $poke_data = array();
-for ($i = 1; $i < 2; ++$i){
+for ($i = 1; $i < 3; ++$i){
   $api_data = $api->pokemon($i);
   $nth_poke = json_decode($api_data);
   $all_poke[$i] = $nth_poke;
-//  $test = $all_poke[$i]->name;
-//  echo($test); 
 }
-for ($i = 1; $i < 2; ++$i){
+for ($i = 1; $i < 3; ++$i){
   $condition = $all_poke[$i]->types[0]->slot;
   if ($condition == 2){
     $nth_data = [
@@ -32,22 +30,6 @@ for ($i = 1; $i < 2; ++$i){
     ];
   }
   $poke_data[$i] = $nth_data;
-  var_dump($poke_data);
 }
-//$first_poke = $api->pokemon('5');
-//$pokemon_data = json_decode($first_poke);
-// $pokemon_name = $pokemon_data->name;
-// $pokemon_height = $pokemon_data->height;
-// $pokemon_weight = $pokemon_data->weight;
-// $pokemon_types = $pokemon_data->types[0]->slot;
-// $pokemon_type = $pokemon_data->types[0]->type->name;
-// echo $pokemon_name, "\n", $pokemon_height, "\n", $pokemon_weight, "\n", $pokemon_types, "\n", $pokemon_type, "\n";
-// $sendout_array = [
-//     "name" => $pokemon_name,
-//     "height" => $pokemon_height,
-//     "weight" => $pokemon_weight,
-//     "types" => $pokemon_types,
-//     "type" => $pokemon_type,
-// ];
-// var_dump($sendout_array);
+echo json_encode($poke_data);
 ?>
